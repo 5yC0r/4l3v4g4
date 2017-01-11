426f88395c39e5160fdb2402fd78a28b9f25656a
Ale <alessia.m.echevarria@gmail.com>
2017-01-11 17:20:53 -0500

    estructura de pagina

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Syar Corp</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="js/main.js">
+</head>
+<body>
+
+	<div class="contenedor-global">
+
+		<!-- bloque uno logo, redes sociales y busqueda -->
+		<div class="menu">	
+		</div>
+		
+		<!-- bloque dos silder -->
+		<div class="slider">
+			
+		</div>
+
+
+		<!-- bloque tres Areas de darrollo  -->
+		<div class="areas">
+			
+		</div>
+
+		<!-- bloque cuatro  publicidad -->
+		<div class="publicidad">
+			
+		</div>
+
+		<!-- bloque cinco descripcion de la empresa con slider-->
+		<div class="descripcion">
+			
+		</div>
+
+		<!-- bloque seis tecnologias -->
+
+		<div class="tecnologias">
+			
+		</div>
+
+		<!-- bloque siete footer -->
+		<div class="footer">
+			
+		</div>
+	</div>
+	
+</body>
+</html>
