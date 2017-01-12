15a6087e6c08625f5c7fd2d7ed1dc9d819589ab9
jflion <jesus.lv.fernandez@gmail.com>
2017-01-12 13:01:06 -0500

    se agrego sombra a menu y cambio js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
 		if ($(this).scrollTop()>10) {
-			$('.menu').addClass('menu-fijo');
-			$('.menu').removeClass('menu');
+			$('.menu').css('position','fixed');
 		}else{
-			$('.menu-fijo').addClass('menu');
-			$('.menu-fijo').removeClass('menu-fijo');
+			$('.menu').css('position','initial');
 		}
 	});
+
 
 	$('#slider').coinslider({ width: 900, height: 400, navigation: false, delay: 3000 });
 });
